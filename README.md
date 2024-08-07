4b96ea40129ba02494f169a91f03fb8c2fa04e88
Julien Kipp <julien@doji-tech.com>
2024-08-08 01:56:20 +0200

    add utils

--- README.md
+++ README.md
@@ -9,9 +9,20 @@ A small collection of utility scripts and extensions for Unity Sentis
 
 ## About
 
-- Brings back an Ops class similar to what was available in previous Sentis versions. Unlike with the IBackend class it has the convenience of not needing to figure out the shape and allocate the output tensor when doing operations. In terms of memory management it will not reuse any memory and the consumer is expected to call Flush() to clear the memory of all the temporary tensors at a time they see fit.
+- Brings back an Ops class similar to what was available in previous Sentis versions. Unlike with the IBackend class it has the convenience of not needing to figure out the shape and allocate the output tensor when doing operations. In terms of memory management it will not reuse any memory and the consumer is responsible to call Flush() to free the memory of all the temporary tensors.
 
-- It also includes some more operations that are either not implemented in Sentis or could do with an API that is more convenient or more pytorch-like for easier porting of pytorch-based pre-/postprocessing code. This includes methods like Concat, RepeatInterleaved, python-like slicing operators, etc.
+- It also includes some more operations that are either not implemented in Sentis or could do with an API that is more convenient or more pytorch-like for easier porting of pytorch-based pre-/postprocessing code. 
+  - Quantile()
+  - Sort()
+  - Concatenate() as an alias for Concat()
+  - overloads for Concatenate that take individual Tensors instead of arrays (e.g. Concat(t1, t1))
+  - RepeatInterleaved (although not implemented for all shapes yet)
+  - Split() that matches the arguments of numpy.split or torch.chunk (i.e. providing a sections parameter rather than requiring start & end parameter). It also takes a preallocated list to store the resulting chunks in.
+  - SplitHalf(): returns the two output tensors as a tuple.
+  - python-like slicing operators: Overloads for the Slice() method that use C# indices and ranges rather than ReadOnlySpan. When you would do tensor[:, -1, :] with a pytorch tensor you can now do ops.Slice(tensor, .., ^1, ..)
+
+Some not-so-great aspects:
+- need to use Reflection to access the internal TensorShape.Split() method to get the shape when splitting tensors
 
 **Used by**
 - [com.doji.transformers]
---
+++ com.doji.sentis-utils/Runtime/Scripts/SentisUtils.cs
@@ -0,0 +1,229 @@
+using System;
+using System.Collections.Generic;
+using Unity.Sentis;
+using UnityEngine.Assertions;
+
+namespace Doji.AI.Utils {
+
+    /// <summary>
+    /// Extends Ops class with not-yet implemented operators
+    /// and some overloads for more convenience.
+    /// </summary>
+    public static class SentisUtils {
+
+        private static Tensor[] _tmpTensorRefs = new Tensor[2];
+
+        /// <summary>
+        /// Computes the q-th quantiles of each row of the input tensor along the dimension dim.
+        /// torch.quantile
+        /// </summary>
+        public static TensorFloat Quantile(this Ops ops, TensorFloat tensor, float q, int dim) {
+            TensorFloat sorted = ops.Sort(tensor, dim);
+
+            if (q < 0 || q > 1) {
+                throw new ArgumentException("Quantile value must be between 0 and 1");
+            }
+
+            float index = (tensor.shape[dim] - 1) * q;
+
+            using TensorInt lowerIndex = new TensorInt((int)MathF.Floor(index));
+            using TensorInt upperIndex = new TensorInt((int)MathF.Ceiling(index));
+
+            TensorFloat lowerValues = ops.Gather(sorted, lowerIndex, dim);
+            TensorFloat upperValues = ops.Gather(sorted, upperIndex, dim);
+            float weights = index - (int)MathF.Floor(index);
+
+            TensorFloat sub = ops.Sub(upperValues, lowerValues);
+            TensorFloat mul = ops.Mul(sub, weights);
+            TensorFloat interpolated = ops.Add(mul, lowerValues);
+            return interpolated;
+        }
+
+        public static TensorFloat Sort(this Ops ops, TensorFloat tensor, int dim) {
+            int num = tensor.shape[dim];
+            return ops.TopK(tensor, num, dim, largest: false /* sort lowest-to-highest */, true).values;
+        }
+
+        public static TensorFloat Clamp(this Ops ops, TensorFloat tensor, TensorFloat min, TensorFloat max) {
+            return ops.Min(ops.Max(tensor, min), max);
+        }
+
+        /// <summary>
+        /// Alias for <see cref="Ops.Concat(Tensor[], int)"/> to match numpy.concatenate()
+        /// naming and for convenience of not needing to create a Tensor array.
+        /// </summary>
+        public static Tensor Concatenate(this Ops ops, Tensor tensor1, Tensor tensor2, int axis = 0) {
+            _tmpTensorRefs[0] = tensor1;
+            _tmpTensorRefs[1] = tensor2;
+            return ops.Concat(_tmpTensorRefs, axis);
+        }
+
+        /// <summary>
+        /// Alias for <see cref="Ops.Concat(Tensor[], int)"/> to match numpy.concatenate()
+        /// naming and for convenience by adding a List<TensorFloat> overload.
+        /// </summary>
+        public static Tensor Concatenate(this Ops ops, List<Tensor> tensors, int axis = 0) {
+            return ops.Concat(tensors, axis);
+        }
+
+        public static TensorFloat Concatenate(this Ops ops, TensorFloat tensor1, TensorFloat tensor2, int axis = 0) {
+            return ops.Concatenate(tensor1 as Tensor, tensor2 as Tensor, axis) as TensorFloat;
+        }
+
+        public static TensorInt Concatenate(this Ops ops, TensorInt tensor1, TensorInt tensor2, int axis = 0) {
+            return ops.Concatenate(tensor1 as Tensor, tensor2 as Tensor, axis) as TensorInt;
+        }
+
+        /// <summary>
+        /// Similar to torch.repeat() or numpy.tile()
+        /// </summary>
+        public static TensorFloat Repeat(this Ops ops, TensorFloat tensor, int repeats, int axis) {
+            if (repeats <= 0) {
+                throw new ArgumentException($"Repeat count must be greater than zero, was {repeats}.", nameof(repeats));
+            }
+
+            if (repeats == 1) {
+                return tensor;
+            }
+
+            int[] r = ArrayUtils.Full(tensor.shape.rank, 1);
+            r[axis] = repeats;
+            return ops.Tile(tensor, r);
+        }
+
+        /// <summary>
+        /// Similar to torch.repeat() or numpy.tile()
+        /// </summary>
+        public static TensorInt Repeat(this Ops ops, TensorInt tensor, int repeats, int axis) {
+            if (repeats <= 0) {
+                throw new ArgumentException($"Repeat count must be greater than zero, was {repeats}.", nameof(repeats));
+            }
+
+            if (repeats == 1) {
+                return tensor;
+            }
+
+            int[] r = ArrayUtils.Full(tensor.shape.rank, 1);
+            r[axis] = repeats;
+            return ops.Tile(tensor, r);
+        }
+
+        /// <summary>
+        /// Similar to torch.repeat_interleave() or numpy.repeat()
+        /// </summary>
+        public static TensorFloat RepeatInterleave(this Ops ops, TensorFloat tensor, int repeats, int dim) {
+            if (repeats <= 0) {
+                throw new ArgumentException($"Repeat count must be greater than zero, was {repeats}.", nameof(repeats));
+            }
+            if (tensor.shape.rank > 1) {
+                throw new ArgumentException($"RepeatInterleave not supported yet for tensors with rank > 1. Shape was {tensor.shape}");
+            }
+
+            // implement repeat_interleave using repeat, reshape & transpose ops
+            var repeat = ops.Repeat(tensor, repeats, dim);
+            var flatShape = new TensorShape(repeat.shape.length);
+            repeat.Reshape(flatShape);
+            repeat.Reshape(new TensorShape(repeats, flatShape.length / repeats));
+            var transpose = ops.Transpose(repeat, new int[] { 1, 0 });
+            transpose.Reshape(flatShape);
+            return transpose;
+        }
+
+        /// <summary>
+        /// Similar to torch.repeat_interleave() or numpy.repeat()
+        /// </summary>
+        public static TensorInt RepeatInterleave(this Ops ops, TensorInt tensor, int repeats, int dim) {
+            if (repeats <= 0) {
+                throw new ArgumentException($"Repeat count must be greater than zero, was {repeats}.", nameof(repeats));
+            }
+            if (tensor.shape.rank > 1) {
+                throw new ArgumentException($"RepeatInterleave not supported yet for tensors with rank > 1. Shape was {tensor.shape}");
+            }
+
+            // implement repeat_interleave using repeat, reshape & transpose ops
+            var repeat = ops.Repeat(tensor, repeats, dim);
+            var flatShape = new TensorShape(repeat.shape.length);
+            repeat.Reshape(flatShape);
+            repeat.Reshape(new TensorShape(repeats, flatShape.length / repeats));
+            var transpose = ops.Transpose(repeat, new int[] { 1, 0 });
+            transpose.Reshape(flatShape);
+            return transpose;
+        }
+
+        /// <summary>
+        /// Alias for <see cref="Ops.Split{T}(T, int, int, int)"/> to match the
+        /// arguments of numpy.split() or torch.chunk() i.e. providing <paramref name="sections"/>
+        /// that the original tensor is split into.
+        /// </summary>
+        public static void Split(this Ops ops, Tensor tensor, int sections, int axis = 0, List<TensorFloat> splitTensors = null) {
+            if (tensor.shape[axis] % sections != 0) {
+                throw new ArgumentException($"Tensor dimension {axis} (length: {tensor.shape[axis]}) can not be divided into {sections} sections.");
+            }
+            splitTensors ??= new List<TensorFloat>();
+            splitTensors.Clear();
+
+            int step = tensor.shape[axis] / sections;
+            int end = tensor.shape[axis] - step;
+            for (int i = 0; i < end; i += step) {
+                var section = ops.Split(tensor, axis: axis, i, i + step) as TensorFloat;
+                splitTensors.Add(section);
+            }
+        }
+
+        /// <summary>
+        /// Splits a tensor into two sections.
+        /// </summary>
+        public static (TensorFloat a, TensorFloat b) SplitHalf(this Ops ops, Tensor tensor, int axis = 0) {
+            if (tensor.shape[axis] % 2 != 0) {
+                throw new ArgumentException($"Tensor dimension {axis} (length: {tensor.shape[axis]}) can not be divided into 2 sections.");
+            }
+            int half = tensor.shape[axis] / 2;
+            int start = 0;
+            int end = tensor.shape[axis];
+            var a = ops.Split(tensor, axis: axis, start, half) as TensorFloat;
+            var b = ops.Split(tensor, axis: axis, half, end) as TensorFloat;
+            return (a, b);
+        }
+
+        internal static T Slice<T>(this Ops ops, T tensor, Index i) where T : Tensor {
+            return ops.Slice(tensor, i.ToRange());
+        }
+        internal static T Slice<T>(this Ops ops, T tensor, Index i, Range r) where T : Tensor {
+            return ops.Slice(tensor, i.ToRange(), r);
+        }
+        internal static T Slice<T>(this Ops ops, T tensor, Range r0, Index i, Range r2) where T : Tensor {
+            T O = ops.Slice(tensor, r0, i.ToRange(), r2);
+            return O;
+        }
+        internal static Range ToRange(this Index i) {
+            return i.IsFromEnd ? i..new Index(i.Value - 1, true) : (i..new Index(i.Value + 1, false));
+        }
+
+        public static T Slice<T>(this Ops ops, T tensor, params Range[] ranges) where T : Tensor {
+            var shape = tensor.shape;
+            int rank = shape.rank;
+            int numDimensionsToSlice = ranges.Length;
+
+            Assert.IsTrue(rank >= 2, $"Too many indices for tensor of dimension {rank}.");
+
+            unsafe {
+                Span<int> starts = stackalloc int[rank];
+                Span<int> ends = stackalloc int[rank];
+                Span<int> axes = stackalloc int[rank];
+                Span<int> steps = stackalloc int[rank];
+
+                for (int i = 0; i < rank; i++) {
+                    Range range = i < numDimensionsToSlice ? ranges[i] : ..;
+                    (int offset, int length) = range.GetOffsetAndLength(shape[i]);
+                    starts[i] = offset;
+                    ends[i] = offset + length;
+                    axes[i] = i;
+                    steps[i] = 1;
+                }
+
+                Tensor outputTensor = ops.Slice(tensor, starts, ends, axes, steps);
+                return outputTensor as T;
+            }
+        }
+    }
+}
