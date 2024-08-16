0a4466b79bd1cbde3935a89c158fae29e97ec105
Julien Kipp <julien@doji-tech.com>
2024-08-16 18:23:16 +0200

    initial release

---
+++ com.doji.sentis-utils/Runtime/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("Doji.SentisUtils")]
+[assembly: AssemblyDescription("A small collection of utility scripts and extensions for Unity Sentis")]
+[assembly: AssemblyCompany("Doji Technologies")]
+[assembly: AssemblyProduct("Doji.SentisUtils")]
+[assembly: AssemblyCopyright("Copyright © Julien Kipp 2024")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0")]
