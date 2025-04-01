11872dfbfa2f94234db052bd9a6d76fb80101197
Julien Kipp <julien@doji-tech.com>
2025-04-01 21:32:01 +0200

    bump version

--- com.doji.sentis-utils/Runtime/AssemblyInfo.cs
+++ com.doji.sentis-utils/Runtime/AssemblyInfo.cs
@@ -6,6 +6,6 @@ using System.Reflection;
 [assembly: AssemblyProduct("Doji.SentisUtils")]
 [assembly: AssemblyCopyright("Copyright © Julien Kipp 2024")]
 
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
-[assembly: AssemblyInformationalVersion("0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0")]
