e6e347b303ee9bb25fd8c7565d23853f0be9d203
Julien Kipp <julien@doji-tech.com>
2026-01-24 13:53:02 +0100

    fix slice tests failing for GPU backend

--- com.doji.sentis-utils/Tests/Editor/SlicingTest.cs
+++ com.doji.sentis-utils/Tests/Editor/SlicingTest.cs
@@ -3,42 +3,68 @@ using Unity.InferenceEngine;
 
 namespace Doji.AI.Editor.Tests {
     public class SlicingTests {
+        private static readonly BackendType[] SupportedBackends =
+        {
+            BackendType.CPU,
+            BackendType.GPUCompute,
+            //BackendType.GPUPixel // slicing requires reshape
+        };
 
-        [Test]
-        public void TestSlicingShape1D() {
-            using Ops ops = new Ops(BackendType.GPUCompute);
+        [TestCaseSource(nameof(SupportedBackends))]
+        public void TestSlicingShape1D(BackendType backend) {
+            using Ops ops = new Ops(backend);
             TensorShape shape = new TensorShape(3);
             using Tensor<int> tensor = GetTensor(shape);
+
             var result = ops.Slice(tensor, 1);
+            ops.ExecuteCommandBufferAndClear();
             result.ReadbackAndClone();
+
             Assert.That(result.shape, Is.EqualTo(new TensorShape()));
-            Assert.That(result.DownloadToArray(), Is.EqualTo(new int[] { 2 }));
+            Assert.That(result.DownloadToArray(), Is.EqualTo(new[] { 2 }));
+            result.Dispose();
+
         }
 
-        [Test]
-        public void TestSlicingShape2D() {
-            using Ops ops = new Ops(BackendType.GPUCompute);
+        [TestCaseSource(nameof(SupportedBackends))]
+        public void TestSlicingShape2D(BackendType backend) {
+            using Ops ops = new Ops(backend);
             TensorShape shape = new TensorShape(2, 3);
             using Tensor<int> tensor = GetTensor(shape);
+
             var result = ops.Slice(tensor, .., 1);
+            ops.ExecuteCommandBufferAndClear();
             result.ReadbackAndClone();
+
             Assert.That(result.shape, Is.EqualTo(new TensorShape(2)));
-            Assert.That(result.DownloadToArray(), Is.EqualTo(new int[] { 2, 5 }));
+            Assert.That(result.DownloadToArray(), Is.EqualTo(new[] { 2, 5 }));
+            result.Dispose();
         }
 
-        [Test]
-        public void TestSlicingShape3D() {
-            using Ops ops = new Ops(BackendType.GPUCompute);
+        [TestCaseSource(nameof(SupportedBackends))]
+        public void TestSlicingShape3D(BackendType backend) {
+            using Ops ops = new Ops(backend);
             TensorShape shape = new TensorShape(2, 3, 4);
             using Tensor<int> tensor = GetTensor(shape);
+
             var result = ops.Slice(tensor, .., 1, ..);
+            ops.ExecuteCommandBufferAndClear();
             result.ReadbackAndClone();
+
             Assert.That(result.shape, Is.EqualTo(new TensorShape(2, 4)));
-            Assert.That(result.DownloadToArray(), Is.EqualTo(new int[] { 5, 6, 7, 8, 17, 18, 19, 20 }));
+            Assert.That(
+                result.DownloadToArray(),
+                Is.EqualTo(new[] { 5, 6, 7, 8, 17, 18, 19, 20 })
+            );
+            result.Dispose();
+
         }
 
         private Tensor<int> GetTensor(TensorShape shape) {
-            return new Tensor<int>(shape, ArrayUtils.Arange(1, shape.length + 1));
+            return new Tensor<int>(
+                shape,
+                ArrayUtils.Arange(1, shape.length + 1)
+            );
         }
     }
 }
